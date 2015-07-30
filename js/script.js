ad380cc0b465c70cf8dd9274ee02722c9123551d
Dmitry <7dmitry77@gmail.com>
2015-07-30 04:42:00 +0000

    Circle

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
  var dataset = [];
- for(var i=0; i<25; i++) {
+ for(var i=0; i<5; i++) {
      var newNumber = Math.floor(Math.random()*30);
      dataset.push(newNumber);
  }
@@ -12,10 +12,32 @@
                     var barHeight = d*10;
                    return barHeight+'px'; 
                 });
-                
+dataset = [5, 10, 15, 20, 30];
 var w = 500;
 var h = 100;
 var svg = d3.select('body')
     .append('svg')
     .attr('width', w)
     .attr('height', h);
+    
+var circles=svg.selectAll("circle")
+                .data(dataset)
+                .enter()
+                .append("circle");
+                
+                
+circles.attr('cx', function(d, i){
+    return (i*50)+25;
+    
+})
+    .attr('cy', h/2)
+    .attr('r', function(i){
+        return i;
+    })
+    .attr("fill", "yellow")
+    .attr("stroke", "orange")
+    .attr("stroke-width", function(d){
+        return d/2;
+    });
+
+    
