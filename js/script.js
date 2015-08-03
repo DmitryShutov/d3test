52b16fdfa657c999ae99db2825530ff6d90359aa
Dmitry <7dmitry77@gmail.com>
2015-08-03 05:30:44 +0000

    yScale doing!

--- js/script.js
+++ js/script.js
@@ -2,9 +2,10 @@ var dataset = [ 5, 10, 13, 19, 21, 25, 22, 18, 15, 13,
                 11, 12, 15, 20, 18, 17, 16, 18, 23, 25];
      
 //New SVG
-var w = 600;
-var h = 250;
+var w = 1200;
+var h = 750;
 var barPadding = 1;
+var padding = 20;
 
 var svg = d3.select("body")
             .append("svg")
@@ -15,7 +16,11 @@ var xScale = d3.scale.ordinal()
                 .domain(d3.range(dataset.length))
                 .rangeRoundBands([0,w], 0.05)
                 
-                            
+var yScale = d3.scale.linear()
+                .domain([0, d3.max(dataset)])
+                .range([h,0]);
+                
+
 svg.selectAll("rect")
     .data(dataset)
     .enter()
@@ -24,14 +29,14 @@ svg.selectAll("rect")
         return xScale(i);
     })
     .attr('y', function(d){
-        return h-d*4;
+        return (yScale(d));
     })
-    .attr('width', 20)
+    .attr('width', xScale.rangeBand())
     .attr('height', function(d){
-        return d*4;
+        return h-(yScale(d));
     })
     .attr("fill", function(d){
-        return "rgb(0, 0, "+ (d*10) + ")";
+        return "rgb("+ (d*10) + ", 0, 0)";
     });
 
 svg.selectAll("text")
@@ -43,47 +48,12 @@ svg.selectAll("text")
 			   })
 			   .attr("text-anchor", "middle")
 			   .attr("x", function(d, i) {
-			   		return (xScale(i)+10);
+			   		return (xScale(i)+xScale.rangeBand()/2);
 			   })
 			   .attr("y", function(d) {
-			   		return h - (d * 4) + 14;
+			   		return yScale(d)+w/30;
 			   })
 			   .attr("font-family", "sans-serif")
-			   .attr("font-size", "11px")
+			   .attr("font-size", "17px")
 			   .attr("fill", "white");
 			   
-			   
-			   
-
-    
-//Old SVG
-/*
-dataset = [5, 10, 15, 20, 30];
-var w = 500;
-var h = 100;
-var svg = d3.select('body')
-    .append('svg')
-    .attr('width', w)
-    .attr('height', h);
-    
-var circles=svg.selectAll("circle")
-                .data(dataset)
-                .enter()
-                .append("circle");
-                
-                
-circles.attr('cx', function(d, i){
-    return (i*50)+25;
-    
-})
-    .attr('cy', h/2)
-    .attr('r', function(i){
-        return i;
-    })
-    .attr("fill", "yellow")
-    .attr("stroke", "orange")
-    .attr("stroke-width", function(d){
-        return d/2;
-    });
-*/
-    
